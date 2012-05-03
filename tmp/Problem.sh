f94ee5478450f2989f3715bfc430c2d5267bccb8
Fougas <Fougas@Flipper>
2012-05-03 23:09:24 +0600

    Added ability to force the number of source files the module is split into. Moved Generator-related classes inside it. Seeded the domain decomposition infrastrucure. (incomplete, not working)

--- tmp/Problem.sh
+++ tmp/Problem.sh
@@ -1,1 +1,1 @@
-gcc -pg -g -ansi -pedantic -Wall -DNDEBUG -O3 $SUPERLU_CPPFLAGS problem_auto1.c problem.c $SUPERLU_LDFLAGS $SUPERLU_LDLIBS
+gcc -g -ansi -pedantic -Wall -DNDEBUG -O3 $SUPERLU_CPPFLAGS problem_auto1.c problem.c $SUPERLU_LDFLAGS $SUPERLU_LDLIBS
