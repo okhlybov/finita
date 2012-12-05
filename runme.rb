56b50602357056ff72bef6382064f4d61620bf02
Oleg A. Khlybov <fougas@mail.ru>
2012-12-05 18:02:11 +0500

    Finished mpi and omp-aware naive mapper and excplicit solver (complex support is still missing). Generated code compiles but bails out with assertion even in sequential mode.

--- runme.rb
+++ runme.rb
@@ -1,14 +1,13 @@
 N = Variable.new(:N, Integer)
 C = Constant.new(:C, 5)
-A = Domain::Cubic::Area.new(N,N,N)
-B = Domain::Cubic::Area.new([1,N-1],[1,N-1],[1,N-1])
+A = Domain::Cubic::Area.new(N,N)
 F = Field.new(:F, Float, A)
 
 Problem.new(:Problem) do |p|
   System.new(:System) do |s|
     s.discretizer = Discretizer::Trivial.new
-    s.solver = Solver::Explicit.new(Mapper::Naive.new, Environment::MPI.new)
-    Assignment.new({C+N=>F}, B)
-    Assignment.new({1=>F}, A)
+    #s.solver = Solver::Explicit.new(Mapper::Naive.new, Environment::MPI.new)
+    s.solver = Solver::Explicit.new(Mapper::Naive.new)
+    Assignment.new({C=>F}, A)
   end
 end
