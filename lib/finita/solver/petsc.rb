eb8aa0b5c858ddc5f22deba815fe9c26769b36b1
Oleg A. Khlybov <fougas@mail.ru>
2013-04-25 18:03:16 +0600

    Fixed linear PETSc solver. It is now functioning correctly.

--- lib/finita/solver/petsc.rb
+++ lib/finita/solver/petsc.rb
@@ -19,6 +19,42 @@ class Solver::PETSc < Solver::Matrix
           ierr = PetscFinalize(); CHKERRQ(ierr);
           FINITA_RETURN(0);
         }
+        typedef struct {
+          PetscInt row, column;
+        } FinitaRowColumn;
+        static int FinitaRowOrientedCompare(const void* l, const void* r) {
+          const FinitaRowColumn* lt = (FinitaRowColumn*)l;
+          const FinitaRowColumn* rt = (FinitaRowColumn*)r;
+          if(lt->row < rt->row)
+            return -1;
+          else if(lt->row > rt->row)
+            return +1;
+          else if(lt->column < rt->column)
+            return -1;
+          else if(lt->column > rt->column)
+            return +1;
+          else
+            return 0;
+        }
+        static int FinitaColumnOrientedCompare(const void* l, const void* r) {
+          FinitaRowColumn* lt = (FinitaRowColumn*)l;
+          FinitaRowColumn* rt = (FinitaRowColumn*)r;
+          if(lt->column < rt->column)
+            return -1;
+          else if(lt->column > rt->column)
+            return +1;
+          else if(lt->row < rt->row)
+            return -1;
+          else if(lt->row > rt->row)
+            return +1;
+          else
+            return 0;
+        }
+        static void FinitaRowColumnSort(FinitaRowColumn* rc, size_t count, int row_first) {
+          #{assert}(rc);
+          #{assert}(count > 0);
+          qsort(rc, count, sizeof(FinitaRowColumn), row_first ? FinitaRowOrientedCompare : FinitaColumnOrientedCompare);
+        }
       $
     end
     def write_initializer(stream)
@@ -46,55 +82,12 @@ class Solver::PETSc < Solver::Matrix
     def write_defs(stream)
       stream << %$static PetscErrorCode #{petscSetup}(void);$;
       super
-      if mpi?
-        mt = "MATMPIAIJ"
-        vt = "VECMPI"
-        preallocate_stmt = %${
-          PetscInt *dnz, *onz;
-          FINITA_ENTER;
-          dnz = (PetscInt*)#{calloc}(size, sizeof(PetscInt)); #{assert}(dnz);
-          onz = (PetscInt*)#{calloc}(size, sizeof(PetscInt)); #{assert}(onz);
-          #{SparsityPatternCode.itCtor}(&it, &#{sparsity});
-          while(#{SparsityPatternCode.itHasNext}(&it)) {
-            size_t row, column;
-            #{NodeCoordCode.type} coord = #{SparsityPatternCode.itNext}(&it);
-            row = #{mapper_code.index}(coord.row);
-            #{assert}(first <= row && row <= last);
-            column = #{mapper_code.index}(coord.column);
-            if(first <= column && column <= last) ++dnz[row - first]; else ++onz[row - first];
-          }
-          ierr = MatMPIAIJSetPreallocation(#{matrix}, 0, dnz, 0, onz); CHKERRQ(ierr);
-          #{free}(dnz);
-          #{free}(onz);
-          FINITA_LEAVE;
-        }$
-      else
-        mt = "MATSEQAIJ"
-        vt = "VECSEQ"
-        preallocate_stmt = %${
-          PetscInt *nnz;
-          FINITA_ENTER;
-          nnz = (PetscInt*)#{calloc}(size, sizeof(PetscInt)); #{assert}(nnz);
-          #{SparsityPatternCode.itCtor}(&it, &#{sparsity});
-          while(#{SparsityPatternCode.itHasNext}(&it)) {
-            size_t row, column;
-            #{NodeCoordCode.type} coord = #{SparsityPatternCode.itNext}(&it);
-            row = #{mapper_code.index}(coord.row);
-            #{assert}(first <= row && row <= last);
-            column = #{mapper_code.index}(coord.column);
-            ++nnz[row - first];
-          }
-          ierr = MatSeqAIJSetPreallocation(#{matrix}, 0, nnz); CHKERRQ(ierr);
-          #{free}(nnz);
-          FINITA_LEAVE;
-        }$
-      end
       if @solver.linear?
         stream << %$static KSP #{ksp};$
         solver_setup_stmt = %$
           ierr = KSPCreate(PETSC_COMM_WORLD, &#{ksp}); CHKERRQ(ierr);
           ierr = KSPSetFromOptions(#{ksp}); CHKERRQ(ierr);
-          ierr = KSPSetOperators(#{ksp}, #{matrix}, #{matrix}, 0); CHKERRQ(ierr);
+          ierr = KSPSetOperators(#{ksp}, #{matrix}, #{matrix}, DIFFERENT_NONZERO_PATTERN); CHKERRQ(ierr);
         $
       else
         stream << %$static SNES #{snes};$
@@ -107,20 +100,61 @@ class Solver::PETSc < Solver::Matrix
         static Mat #{matrix};
         static Vec #{vector};
         static size_t #{matrixSize}, #{vectorSize};
-        static PetscInt *#{matrixRows}, *#{matrixColumns}, *#{vectorIndices};
-        static PetscScalar* #{matrixValues}, *#{vectorValues};
+        static FinitaRowColumn* #{matrixRC};
+        static PetscInt *#{vectorIndices};
         static PetscErrorCode #{petscSetup}(void) {
           PetscErrorCode ierr;
           #{SparsityPatternCode.it} it;
           size_t size, index, first, last;
+          const MatType mat_type;
           FINITA_ENTER;
           size = #{decomposer_code.indexCount}();
           first = #{decomposer_code.firstIndex}();
           last = #{decomposer_code.lastIndex}();
           ierr = MatCreate(PETSC_COMM_WORLD, &#{matrix}); CHKERRQ(ierr);
-          ierr = MatSetSizes(#{matrix}, size, #{mapper_code.size}(), PETSC_DECIDE, PETSC_DECIDE); CHKERRQ(ierr);
-          ierr = MatSetType(#{matrix}, #{mt}); CHKERRQ(ierr);
-          #{preallocate_stmt};
+          ierr = MatSetSizes(#{matrix}, size, PETSC_DECIDE, PETSC_DETERMINE, #{mapper_code.size}()); CHKERRQ(ierr);
+          ierr = MatSetFromOptions(#{matrix}); CHKERRQ(ierr);
+          ierr = MatGetType(#{matrix}, &mat_type); CHKERRQ(ierr);
+          if(strcmp(mat_type, MATSEQAIJ) == 0) {
+            PetscInt *nnz;
+            FINITA_ENTER;
+            nnz = (PetscInt*)#{calloc}(size, sizeof(PetscInt)); #{assert}(nnz);
+            #{SparsityPatternCode.itCtor}(&it, &#{sparsity});
+            while(#{SparsityPatternCode.itHasNext}(&it)) {
+              size_t row, column;
+              #{NodeCoordCode.type} coord = #{SparsityPatternCode.itNext}(&it);
+              row = #{mapper_code.index}(coord.row);
+              #{assert}(first <= row && row <= last);
+              column = #{mapper_code.index}(coord.column);
+              ++nnz[row - first];
+            }
+            ierr = MatSeqAIJSetPreallocation(#{matrix}, PETSC_DEFAULT, nnz); CHKERRQ(ierr);
+            #{free}(nnz);
+            FINITA_LEAVE;
+          } else
+          if(strcmp(mat_type, MATMPIAIJ) == 0) {
+            PetscInt *dnz, *onz;
+            FINITA_ENTER;
+            dnz = (PetscInt*)#{calloc}(size, sizeof(PetscInt)); #{assert}(dnz);
+            onz = (PetscInt*)#{calloc}(size, sizeof(PetscInt)); #{assert}(onz);
+            #{SparsityPatternCode.itCtor}(&it, &#{sparsity});
+            while(#{SparsityPatternCode.itHasNext}(&it)) {
+              size_t row, column;
+              #{NodeCoordCode.type} coord = #{SparsityPatternCode.itNext}(&it);
+              row = #{mapper_code.index}(coord.row);
+              #{assert}(first <= row && row <= last);
+              column = #{mapper_code.index}(coord.column);
+              if(first <= column && column <= last) ++dnz[row - first]; else ++onz[row - first];
+            }
+            ierr = MatMPIAIJSetPreallocation(#{matrix}, PETSC_DEFAULT, dnz, PETSC_DEFAULT, onz); CHKERRQ(ierr);
+            #{free}(dnz);
+            #{free}(onz);
+            FINITA_LEAVE;
+          } else {
+            ierr = MatSetUp(#{matrix}); CHKERRQ(ierr);
+          }
+          ierr = MatSetOption(#{matrix}, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_TRUE); CHKERRQ(ierr);
+          ierr = MatSetOption(#{matrix}, MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_TRUE); CHKERRQ(ierr);
           #ifndef NDEBUG
           {
             PetscInt petsc_first, petsc_last;
@@ -130,25 +164,25 @@ class Solver::PETSc < Solver::Matrix
           }
           #endif
           ierr = VecCreate(PETSC_COMM_WORLD, &#{vector}); CHKERRQ(ierr);
-          ierr = VecSetType(#{vector}, #{vt}); CHKERRQ(ierr);
           ierr = VecSetSizes(#{vector}, size, PETSC_DECIDE); CHKERRQ(ierr);
+          ierr = VecSetFromOptions(#{vector}); CHKERRQ(ierr);
           #{solver_setup_stmt};
-          index = 0;
-          #{matrixSize} = #{SparsityPatternCode.size}(&#{sparsity});
-          #{matrixRows} = (PetscInt*)#{malloc}(#{matrixSize}*sizeof(PetscInt)); #{assert}(#{matrixRows});
-          #{matrixColumns} = (PetscInt*)#{malloc}(#{matrixSize}*sizeof(PetscInt)); #{assert}(#{matrixColumns});
-          #{matrixValues} = (PetscScalar*)#{malloc}(#{matrixSize}*sizeof(PetscScalar)); #{assert}(#{matrixValues});
-          #{SparsityPatternCode.itCtor}(&it, &#{sparsity});
-          while(#{SparsityPatternCode.itHasNext}(&it)) {
-            #{NodeCoordCode.type} coord = #{SparsityPatternCode.itNext}(&it);
-            #{matrixRows}[index] = #{mapper_code.index}(coord.row);
-            #{matrixColumns}[index] = #{mapper_code.index}(coord.column);
-            ++index;
+          {
+            index = 0;
+            #{matrixSize} = #{SparsityPatternCode.size}(&#{sparsity});
+            #{matrixRC} = (FinitaRowColumn*)#{malloc}(#{matrixSize}*sizeof(FinitaRowColumn)); #{assert}(#{matrixRC});
+            #{SparsityPatternCode.itCtor}(&it, &#{sparsity});
+            while(#{SparsityPatternCode.itHasNext}(&it)) {
+              #{NodeCoordCode.type} coord = #{SparsityPatternCode.itNext}(&it);
+              #{matrixRC}[index].row = #{mapper_code.index}(coord.row);
+              #{matrixRC}[index].column = #{mapper_code.index}(coord.column);
+              ++index;
+            }
+            #{assert}(index == #{matrixSize});
+            FinitaRowColumnSort(#{matrixRC}, #{matrixSize}, 1);
           }
-          #{assert}(index == #{matrixSize});
           #{vectorSize} = size;
           #{vectorIndices} = (PetscInt*)#{malloc}(#{vectorSize}*sizeof(PetscInt)); #{assert}(#{vectorIndices});
-          #{vectorValues} = (PetscScalar*)#{malloc}(#{vectorSize}*sizeof(PetscScalar)); #{assert}(#{vectorValues});
           for(index = first; index <= last; ++index) {
             #{vectorIndices}[index - first] = index;
           }
@@ -165,29 +199,48 @@ class Solver::PETSc < Solver::Matrix
     def write_solve_linear(stream)
       stream << %$
         static PetscErrorCode #{invoke}(void) {
-          size_t index;
+          size_t index, count;
+          PetscScalar* values;
+          PetscInt row;
+          PetscInt* columns;
           PetscErrorCode ierr;
           FINITA_ENTER;
-          for(index = 0; index < #{matrixSize}; ++index) {
-            #{matrixValues}[index] = #{lhs_code.evaluate}(#{mapper_code.node}(#{matrixRows}[index]), #{mapper_code.node}(#{matrixColumns}[index]));
+          values = (PetscScalar*)#{malloc}(#{matrixSize}*sizeof(PetscScalar)); #{assert}(values);
+          /*** start a per row matrix fill */
+          columns = (PetscInt*)#{malloc}(#{matrixSize}*sizeof(PetscInt)); #{assert}(columns);
+          for(index = count = 0, row = #{matrixRC}[index].row; index < #{matrixSize}; ++index) {
+            /* assuming RC is in the row-first form */
+            if(#{matrixRC}[index].row > row) {
+              ierr = MatSetValues(#{matrix}, 1, &row, count, columns, values, INSERT_VALUES); CHKERRQ(ierr);
+              row = #{matrixRC}[index].row;
+              count = 0;
+            }
+            #{assert}(count < #{matrixSize});
+            values[count] = #{lhs_code.evaluate}(#{mapper_code.node}(#{matrixRC}[index].row), #{mapper_code.node}(#{matrixRC}[index].column));
+            columns[count] = #{matrixRC}[index].column;
+            ++count;
           }
-          ierr = MatSetValues(#{matrix}, #{matrixSize}, #{matrixRows}, #{matrixSize}, #{matrixColumns}, #{matrixValues}, INSERT_VALUES); CHKERRQ(ierr);
+          ierr = MatSetValues(#{matrix}, 1, &row, count, columns, values, INSERT_VALUES); CHKERRQ(ierr);
+          #{free}(columns);
+          /*** end matrix fill */
           ierr = MatAssemblyBegin(#{matrix}, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
           ierr = MatAssemblyEnd(#{matrix}, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-          ierr = MatSetOption(#{matrix}, MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_TRUE); CHKERRQ(ierr);
-          ierr = MatSetOption(#{matrix}, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_TRUE); CHKERRQ(ierr);
+          #{assert}(#{matrixSize} >= #{vectorSize});
+          /*** start vector fill */
           for(index = 0; index < #{vectorSize}; ++index) {
-            #{vectorValues}[index] = -#{rhs_code.evaluate}(#{mapper_code.node}(#{vectorIndices}[index]));
+            values[index] = -#{rhs_code.evaluate}(#{mapper_code.node}(#{vectorIndices}[index]));
           }
-          ierr = VecSetValues(#{vector}, #{vectorSize}, #{vectorIndices}, #{vectorValues}, INSERT_VALUES); CHKERRQ(ierr);
+          ierr = VecSetValues(#{vector}, #{vectorSize}, #{vectorIndices}, values, INSERT_VALUES); CHKERRQ(ierr);
+          /*** end vector fill */
           ierr = VecAssemblyBegin(#{vector}); CHKERRQ(ierr);
           ierr = VecAssemblyEnd(#{vector}); CHKERRQ(ierr);
           ierr = KSPSolve(#{ksp}, #{vector}, #{vector});
-          ierr = VecGetValues(#{vector}, #{vectorSize}, #{vectorIndices}, #{vectorValues}); CHKERRQ(ierr);
+          ierr = VecGetValues(#{vector}, #{vectorSize}, #{vectorIndices}, values); CHKERRQ(ierr);
           for(index = 0; index < #{vectorSize}; ++index) {
-            #{mapper_code.indexSet}(#{vectorIndices}[index], #{vectorValues}[index]);
+            #{mapper_code.indexSet}(#{vectorIndices}[index], values[index]);
           }
           #{decomposer_code.synchronizeUnknowns}();
+          #{free}(values);
           FINITA_RETURN(0);
         }
       $
