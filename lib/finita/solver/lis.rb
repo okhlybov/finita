39a3f33a0ac0d93218967658c4af1b22681387ed
Oleg A. Khlybov <fougas@mail.ru>
2013-11-27 00:26:35 +0600

    Fixes to the LIS solver. It's now able to pass sample Poisson problem in sequential, multithreaded and MPI modes.

--- lib/finita/solver/lis.rb
+++ lib/finita/solver/lis.rb
@@ -34,43 +34,20 @@ class Solver::LIS < Solver::Matrix
       system_code.finalizer_codes << self
       @have_array = mpi? && !@solver.linear?
       @numeric_array_code = NumericArrayCode[system_code.result] if @have_array
+      @comm = mpi? ? :MPI_COMM_WORLD : 0
     end
     def entities
       @entities.nil? ? @entities = super + [@numeric_array_code].compact : @entities
     end
     def write_setup_body(stream)
       super
-      comm = mpi? ? :MPI_COMM_WORLD : 0
-      stream << %${
-        LIS_INT ierr;
-        ierr = lis_solver_create(&#{solver}); CHKERR(ierr);
-        ierr = lis_solver_set_optionC(#{solver}); CHKERR(ierr);
-        ierr = lis_matrix_create(#{comm}, &#{a}); CHKERR(ierr);
-        ierr = lis_matrix_set_size(#{a}, #{decomposer_code.indexCount}(), 0); CHKERR(ierr);
-        ierr = lis_matrix_set_type(#{a}, LIS_MATRIX_CSR); CHKERR(ierr);
-        ierr = lis_vector_create(#{comm}, &#{x}); CHKERR(ierr);
-        ierr = lis_vector_set_size(#{x}, #{decomposer_code.indexCount}(), 0); CHKERR(ierr);
-        ierr = lis_vector_duplicate(#{x}, &#{b}); CHKERR(ierr);
-      }$
       stream << %$#{@numeric_array_code.ctor}(&#{array}, #{mapper_code.size}());$ if @have_array
     end
     def write_cleanup_body(stream)
       super
-      stream << %${
-        LIS_INT ierr;
-        ierr = lis_solver_destroy(#{solver}); CHKERR(ierr);
-        ierr = lis_matrix_destroy(#{a}); CHKERR(ierr);
-        ierr = lis_vector_destroy(#{x}); CHKERR(ierr);
-        ierr = lis_vector_destroy(#{b}); CHKERR(ierr);
-      }$
       stream << %$#{@numeric_array_code.dtor}(&#{array});$ if @have_array
     end
     def write_defs(stream)
-      stream << %$
-        static LIS_MATRIX #{a};
-        static LIS_VECTOR #{b}, #{x};
-        static LIS_SOLVER #{solver};
-      $
       stream << %$static #{@numeric_array_code.type} #{array};$ if @have_array
       super
       @solver.linear? ? write_solve_linear(stream) : write_solve_nonlinear(stream)
@@ -80,15 +57,29 @@ class Solver::LIS < Solver::Matrix
         void #{system_code.solve}(void) {
           LIS_INT ierr, first, last, index;
           #{SparsityPatternCode.it} it;
+          LIS_SOLVER solver;
+          LIS_MATRIX A;
+          LIS_VECTOR b, x;
           FINITA_ENTER;
+          ierr = lis_solver_create(&solver); CHKERR(ierr);
+          ierr = lis_solver_set_option("-initx_zeros false", solver); CHKERR(ierr);
+          #ifndef NDEBUG
+            ierr = lis_solver_set_option("-print mem", solver); CHKERR(ierr);
+          #endif
+          ierr = lis_solver_set_optionC(solver); CHKERR(ierr);
+          ierr = lis_matrix_create(#{@comm}, &A); CHKERR(ierr);
+          ierr = lis_matrix_set_size(A, #{decomposer_code.indexCount}(), 0); CHKERR(ierr);
+          ierr = lis_vector_create(#{@comm}, &x); CHKERR(ierr);
+          ierr = lis_vector_set_size(x, #{decomposer_code.indexCount}(), 0); CHKERR(ierr);
+          ierr = lis_vector_duplicate(x, &b); CHKERR(ierr);
           first = #{decomposer_code.firstIndex}();
           last = #{decomposer_code.lastIndex}();
           #ifndef NDEBUG
           {
             LIS_INT is, ie;
-            ierr = lis_vector_get_range(#{b}, &is, &ie); CHKERR(ierr);
+            ierr = lis_vector_get_range(b, &is, &ie); CHKERR(ierr);
             FINITA_ASSERT(first == is && last == ie-1);
-            ierr = lis_vector_get_range(#{x}, &is, &ie); CHKERR(ierr);
+            ierr = lis_vector_get_range(x, &is, &ie); CHKERR(ierr);
             FINITA_ASSERT(first == is && last == ie-1);
           }
           #endif
@@ -96,18 +87,24 @@ class Solver::LIS < Solver::Matrix
           while(#{SparsityPatternCode.itHasNext}(&it)) {
             #{NodeCoordCode.type} coord;
             coord = #{SparsityPatternCode.itNext}(&it);
-            ierr = lis_matrix_set_value(LIS_INS_VALUE, #{mapper_code.index}(coord.row), #{mapper_code.index}(coord.column), #{lhs_code.evaluate}(coord.row, coord.column), #{a}); CHKERR(ierr);
+            ierr = lis_matrix_set_value(LIS_INS_VALUE, #{mapper_code.index}(coord.row), #{mapper_code.index}(coord.column), #{lhs_code.evaluate}(coord.row, coord.column), A); CHKERR(ierr);
           }
+          ierr = lis_matrix_assemble(A); CHKERR(ierr);
           for(index = first; index <= last; ++index) {
-            ierr = lis_vector_set_value(LIS_INS_VALUE, index, -#{rhs_code.evaluate}(#{mapper_code.node}(index)), #{b}); CHKERR(ierr);
+            ierr = lis_vector_set_value(LIS_INS_VALUE, index, -#{rhs_code.evaluate}(#{mapper_code.node}(index)), b); CHKERR(ierr);
+            ierr = lis_vector_set_value(LIS_INS_VALUE, index, #{mapper_code.indexGet}(index), x); CHKERR(ierr);
           }
-          ierr = lis_solve(#{a}, #{b}, #{x}, #{solver}); CHKERR(ierr);
+          ierr = lis_solve(A, b, x, solver); CHKERR(ierr);
           for(index = first; index <= last; ++index) {
             LIS_SCALAR value;
-            ierr = lis_vector_get_value(#{x}, index, &value); CHKERR(ierr);
+            ierr = lis_vector_get_value(x, index, &value); CHKERR(ierr);
             #{mapper_code.indexSet}(index, value);
           }
           #{decomposer_code.synchronizeUnknowns}();
+          ierr = lis_solver_destroy(solver); CHKERR(ierr);
+          ierr = lis_matrix_destroy(A); CHKERR(ierr);
+          ierr = lis_vector_destroy(x); CHKERR(ierr);
+          ierr = lis_vector_destroy(b); CHKERR(ierr);
           FINITA_LEAVE;
         }
       $
@@ -120,63 +117,81 @@ class Solver::LIS < Solver::Matrix
           LIS_INT ierr, first, last, index;
           size_t size;
           #{SparsityPatternCode.it} it;
+          LIS_SOLVER solver;
+          LIS_MATRIX A;
+          LIS_VECTOR b, x;
           FINITA_ENTER;
           size = #{mapper_code.size}();
           first = #{decomposer_code.firstIndex}();
           last = #{decomposer_code.lastIndex}();
-          #ifndef NDEBUG
-          {
-            LIS_INT is, ie;
-            ierr = lis_vector_get_range(#{b}, &is, &ie); CHKERR(ierr);
-            FINITA_ASSERT(first == is && last == ie-1);
-            ierr = lis_vector_get_range(#{x}, &is, &ie); CHKERR(ierr);
-            FINITA_ASSERT(first == is && last == ie-1);
-          }
-          #endif
           do {
-            double norm, base = 0, delta = 0; /* TODO : complex */
+            double norm, base = 0, delta = 0, base_, delta_; /* TODO : complex */
+            ierr = lis_solver_create(&solver); CHKERR(ierr);
+            #ifndef NDEBUG
+              ierr = lis_solver_set_option("-print mem", solver); CHKERR(ierr);
+            #endif
+            ierr = lis_solver_set_optionC(solver); CHKERR(ierr);
+            ierr = lis_matrix_create(#{@comm}, &A); CHKERR(ierr);
+            ierr = lis_matrix_set_size(A, #{decomposer_code.indexCount}(), 0); CHKERR(ierr);
+            ierr = lis_vector_create(#{@comm}, &x); CHKERR(ierr);
+            ierr = lis_vector_set_size(x, #{decomposer_code.indexCount}(), 0); CHKERR(ierr);
+            ierr = lis_vector_duplicate(x, &b); CHKERR(ierr);
+            #ifndef NDEBUG
+            {
+              LIS_INT is, ie;
+              ierr = lis_vector_get_range(b, &is, &ie); CHKERR(ierr);
+              FINITA_ASSERT(first == is && last == ie-1);
+              ierr = lis_vector_get_range(x, &is, &ie); CHKERR(ierr);
+              FINITA_ASSERT(first == is && last == ie-1);
+            }
+            #endif
             #{SparsityPatternCode.itCtor}(&it, &#{sparsity});
             while(#{SparsityPatternCode.itHasNext}(&it)) {
               #{NodeCoordCode.type} coord;
               coord = #{SparsityPatternCode.itNext}(&it);
-              ierr = lis_matrix_set_value(LIS_INS_VALUE, #{mapper_code.index}(coord.row), #{mapper_code.index}(coord.column), #{jacobian_code.evaluate}(coord.row, coord.column), #{a}); CHKERR(ierr);
+              ierr = lis_matrix_set_value(LIS_INS_VALUE, #{mapper_code.index}(coord.row), #{mapper_code.index}(coord.column), #{jacobian_code.evaluate}(coord.row, coord.column), A); CHKERR(ierr);
             }
+            ierr = lis_matrix_assemble(A); CHKERR(ierr);
             for(index = first; index <= last; ++index) {
-              ierr = lis_vector_set_value(LIS_INS_VALUE, index, -#{residual_code.evaluate}(#{mapper_code.node}(index)), #{b}); CHKERR(ierr);
+              ierr = lis_vector_set_value(LIS_INS_VALUE, index, -#{residual_code.evaluate}(#{mapper_code.node}(index)), b); CHKERR(ierr);
             }
-            ierr = lis_solve(#{a}, #{b}, #{x}, #{solver}); CHKERR(ierr);
+            ierr = lis_solve(A, b, x, solver); CHKERR(ierr);
       $
       stream << %$
         for(index = first; index <= last; ++index) {
-          LIS_SCALAR dx;
-          #{system_code.cresult} x;
-          x = #{mapper_code.indexGet}(index);
-          ierr = lis_vector_get_value(#{x}, index, &dx); CHKERR(ierr);
-          base += #{abs}(x);
-          delta += #{abs}(dx);
-          #{mapper_code.indexSet}(index, x + dx);
+          LIS_SCALAR dvalue;
+          #{system_code.cresult} value;
+          value = #{mapper_code.indexGet}(index);
+          ierr = lis_vector_get_value(x, index, &dvalue); CHKERR(ierr);
+          base += #{abs}(value);
+          delta += #{abs}(dvalue);
+          #{mapper_code.indexSet}(index, value + dvalue);
         }
         #{decomposer_code.synchronizeUnknowns}();
+        ierr = lis_solver_destroy(solver); CHKERR(ierr);
+        ierr = lis_matrix_destroy(A); CHKERR(ierr);
+        ierr = lis_vector_destroy(x); CHKERR(ierr);
+        ierr = lis_vector_destroy(b); CHKERR(ierr);
       $
       if mpi?
         stream << %$
-          ierr = MPI_Reduce(&base, &base, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); #{assert}(ierr == MPI_SUCCESS);
-          ierr = MPI_Reduce(&delta, &delta, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); #{assert}(ierr == MPI_SUCCESS);
+          ierr = MPI_Reduce(&base, &base_, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); #{assert}(ierr == MPI_SUCCESS);
+          ierr = MPI_Reduce(&delta, &delta_, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); #{assert}(ierr == MPI_SUCCESS);
         $
       end
       stream << %$
-        norm = !step || base == 0 ? 1 : delta/base;
+        norm = !step || base == 0 ? 1 : delta_ / base_;
         stop = norm < #{@solver.rtol};
         #ifndef NDEBUG
           FINITA_HEAD {
-            printf("norm=%d\\n", norm);
+            printf("norm=%e\\n", norm);
             fflush(stdout);
           }
         #endif
       $
       if mpi?
         stream << %$
-          ierr = MPI_Bcast(&stop, &stop, MPI_INT, 0, MPI_COMM_WORLD); #{assert}(ierr == MPI_SUCCESS);
+          ierr = MPI_Bcast(&stop, 1, MPI_INT, 0, MPI_COMM_WORLD); #{assert}(ierr == MPI_SUCCESS);
         $
       end
       stream << %$
