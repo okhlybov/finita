f6e9e11a854f25aef46c80879df7bf3af1471141
Oleg A. Khlybov <fougas@mail.ru>
2022-05-06 19:14:42 +0500

    Added instances handling test.

--- tmp/test.rb
+++ tmp/test.rb
@@ -1,5 +1,19 @@
-require 'autoc/module'
+
+if false
+require 'symbolica/core'
+require 'symbolica/render'
+
+using Symbolica::Refinements
+puts Symbolica::Render::Ruby.render(2i*3-:x**-3)
+end
+
+require 'finita/field'
+require 'finita/module'
 
 
-AutoC::Module.render(:test) do |x|
+g1 = Finita::Grid::Cartesian2.new(:C2)
+f1 = Finita::Field.new(g1, :double, type: :C2F)
+
+Finita::Module.render(:test) do |x|
+  x << f1.instance(:F).create(g1.instance(:g1).create_n(5, 5), 2)
 end
