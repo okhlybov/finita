497c610de40d1a057948477dbc12174aa98b2956
Oleg A. Khlybov <fougas@mail.ru>
2023-10-17 13:59:54 +0500

    Seed a few Fortran matrix tests.

---
+++ etc/f1.f90
@@ -0,0 +1,28 @@
+! gfortran -Ofast -march=native -ffast-math
+! ifort -g -O3 -fp-model fast -xhost -qopt-report=5
+
+program f1
+
+integer, parameter :: N = 1024, T = 10000
+
+integer x, y, q
+
+real(8), dimension(N,N) :: f, f_
+
+do y=1,N
+  f(1,y) = 1
+  f_(1,y) = 1
+end do
+
+do q=1,T
+  do y=2,N-1
+    do x=2,N-1
+      f_(x,y) = ( (f(x+1,y) + f(x-1,y) + f(x,y+1) + f(x,y-1) - 4*f(x,y)) + f(x,y) )*0.1
+    end do
+  end do
+  f = f_
+end do
+
+print *, f_(2,2)
+
+end program
